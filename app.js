5fcdc974df0676bec853f2a4a8ef9eb92bf87574
Ouweshs28 <ouweshseeroo@gmail.com>
2020-04-08 05:08:53 +0400

    dynamic review accordingly

--- app.js
+++ app.js
@@ -16,6 +16,9 @@ const connectionPool = mysql.createPool({
 //Create express app and configure it with body-parser
 const app = express();
 app.use(bodyParser.json());
+let reviews=[];
+let tutorArray=[];
+
 
 app.use(express.static('tutorwebapp'));
 app.use('/', express.static(__dirname + '/www')); // redirect root
@@ -24,6 +27,41 @@ app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redi
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
 app.use('/js', express.static(__dirname + '/node_modules/toastr')); // redirect CSS bootstrap
 app.use('/css', express.static(__dirname + '/node_modules/toastr/build')); // redirect CSS bootstrap
+
+async function getTutorReviews(id){
+    //Build query
+    let sql = "SELECT * FROM Review WHERE tutorID="+id;
+
+    //Wrap the execution of the query in a promise
+    return new Promise ( (resolve, reject) => {
+        connectionPool.query(sql, (err, result) => {
+            if (err){//Check for errors
+                reject("Error executing query: " + JSON.stringify(err));
+            }
+            else{//Resolve promise with results
+                resolve(result);
+            }
+        });
+    });
+}
+
+function ReviewTutorPost(request, response){
+    //Output the data sent to the server
+    let tutor = request.body;
+    console.log("Data received: " + JSON.stringify(tutor));
+
+    //Performing query
+    getTutorReviews(tutor.id).then ( result => {
+        //Output reviews.
+       reviews=(JSON.stringify(result));
+       response.send(reviews);
+
+    }).catch( err => {//Handle the error
+        console.error(JSON.stringify(err));
+    });
+    //Finish off the interaction.
+
+}
 
 function RegStudentPost(request, response){
     //Output the data sent to the server
@@ -75,9 +113,66 @@ function RegTutorPost(request, response){
     response.send("success");
 }
 
+
+/* Returns a promise to get tutors. */
+async function getTutors(){
+    //Build query
+    let sql = "SELECT * FROM Tutor";
+
+    //Wrap the execution of the query in a promise
+    return new Promise ( (resolve, reject) => {
+        connectionPool.query(sql, (err, result) => {
+            if (err){//Check for errors
+                reject("Error executing query: " + JSON.stringify(err));
+            }
+            else{//Resolve promise with results
+                resolve(result);
+            }
+        });
+    });
+}
+
+
+function handleGetRequestTutor(request, response){
+
+    //Split the path of the request into its components
+    let pathArray = request.url.split("/");
+
+    //Get the last part of the path
+    let pathEnd = pathArray[pathArray.length - 1];
+
+    //If path ends with 'users' we return all users
+    if(pathEnd === 'tutors'){
+        getTutors().then ( result => {
+            //Output employees.
+            tutorArray=JSON.stringify(result);
+            //Do something else
+            response.send(tutorArray);
+
+
+        }).catch( err => {//Handle the error
+            console.error(JSON.stringify(err));
+        });
+
+
+    }
+
+    //If the last part of the path is a valid user id, return data about that user
+    else if(pathEnd in tutorArray){
+        response.send(tutorArray[pathEnd]);
+    }
+
+    //The path is not recognized. Return an error message
+}
+
+
+//Set up application to handle GET requests sent to the user path
+app.get('/tutor/*', handleGetRequestTutor);//Returns user with specified ID
+app.get('/tutors', handleGetRequestTutor);//Returns all users
 //Set up application to handle POST requests sent to the user path
 app.post('/registerstudent', RegStudentPost);//Adds a new student user
 app.post('/registertutor', RegTutorPost);//Adds a new tutor user
+app.post('/reviewstutor', ReviewTutorPost);// Searches for corresponding reviews and displays it
 
 app.listen(9000);
 
--- tutorwebapp/client.js
+++ tutorwebapp/client.js
@@ -1,149 +1,226 @@
 //Points to a div element where user combo will be inserted.
-let userDiv;
+let tutorDiv;
+let reviewTutorDiv;
 let addUserResultDiv;
-let homePage=document;
+let homePage = document;
 let loginStudentPage;
-let loginTutorPage=document;
+let loginTutorPage = document;
 let registerStudentPage;
 let registerTutorPage;
 let tutorReviewPage;
 let addReviewPage;
 let tutorPage;
+let usrArr;
 
 //Set up page when window has loaded
 window.onload = init;
 
 //Get pointers to parts of the DOM after the page has loaded.
-function init(){
-    homePage=document.getElementById("home");
-    loginStudentPage=document.getElementById("studentSignIn");
-    loginTutorPage=document.getElementById("tutorSignIn");
-    registerStudentPage=document.getElementById("studentSignUp");
-    registerTutorPage=document.getElementById("tutorSignUp");
-    tutorReviewPage=document.getElementById("tutorReview");
-    addReviewPage=document.getElementById("addReview");
-    tutorPage=document.getElementById("tutors");
+function init() {
+    homePage = document.getElementById("home");
+    loginStudentPage = document.getElementById("studentSignIn");
+    loginTutorPage = document.getElementById("tutorSignIn");
+    registerStudentPage = document.getElementById("studentSignUp");
+    registerTutorPage = document.getElementById("tutorSignUp");
+    tutorReviewPage = document.getElementById("tutorReview");
+    addReviewPage = document.getElementById("addReview");
+    tutorPage = document.getElementById("tutors");
+    tutorDiv = document.getElementById("tutors");
+    reviewTutorDiv = document.getElementById("tutorReview");
     loadHome()
 }
 
-function loadHome(){
-    homePage.style.display="block";
-    loginStudentPage.style.display="none";
-    loginTutorPage.style.display="none";
-    registerStudentPage.style.display="none";
-    registerTutorPage.style.display="none";
-    tutorReviewPage.style.display="none";
-    addReviewPage.style.display="none";
-    tutorPage.style.display="none";
+function loadHome() {
+    homePage.style.display = "block";
+    loginStudentPage.style.display = "none";
+    loginTutorPage.style.display = "none";
+    registerStudentPage.style.display = "none";
+    registerTutorPage.style.display = "none";
+    tutorReviewPage.style.display = "none";
+    addReviewPage.style.display = "none";
+    tutorPage.style.display = "none";
 
 }
 
 function loadStudentLogin() {
-    homePage.style.display="none";
-    loginTutorPage.style.display="none";
-    loginStudentPage.style.display="block";
-    registerTutorPage.style.display="none";
-    registerStudentPage.style.display="none";
-    tutorReviewPage.style.display="none";
-    addReviewPage.style.display="none";
-    tutorPage.style.display="none";
+    homePage.style.display = "none";
+    loginTutorPage.style.display = "none";
+    loginStudentPage.style.display = "block";
+    registerTutorPage.style.display = "none";
+    registerStudentPage.style.display = "none";
+    tutorReviewPage.style.display = "none";
+    addReviewPage.style.display = "none";
+    tutorPage.style.display = "none";
 
 }
 
 function loadTutorLogin() {
-    homePage.style.display="none";
-    loginTutorPage.style.display="block";
-    loginStudentPage.style.display="none";
-    registerTutorPage.style.display="none";
-    registerStudentPage.style.display="none";
-    tutorReviewPage.style.display="none";
-    addReviewPage.style.display="none";
-    tutorPage.style.display="none";
+    homePage.style.display = "none";
+    loginTutorPage.style.display = "block";
+    loginStudentPage.style.display = "none";
+    registerTutorPage.style.display = "none";
+    registerStudentPage.style.display = "none";
+    tutorReviewPage.style.display = "none";
+    addReviewPage.style.display = "none";
+    tutorPage.style.display = "none";
 }
 
 function loadStudentReg() {
-    homePage.style.display="none";
-    loginTutorPage.style.display="none";
-    loginStudentPage.style.display="none";
-    registerTutorPage.style.display="none";
-    registerStudentPage.style.display="block";
-    tutorReviewPage.style.display="none";
-    addReviewPage.style.display="none";
-    tutorPage.style.display="none";
+    homePage.style.display = "none";
+    loginTutorPage.style.display = "none";
+    loginStudentPage.style.display = "none";
+    registerTutorPage.style.display = "none";
+    registerStudentPage.style.display = "block";
+    tutorReviewPage.style.display = "none";
+    addReviewPage.style.display = "none";
+    tutorPage.style.display = "none";
 }
 
 function loadTutorReg() {
-    homePage.style.display="none";
-    loginTutorPage.style.display="none";
-    loginStudentPage.style.display="none";
-    registerTutorPage.style.display="block";
-    registerStudentPage.style.display="none";
-    tutorReviewPage.style.display="none";
-    addReviewPage.style.display="none";
-    tutorPage.style.display="none";
+    homePage.style.display = "none";
+    loginTutorPage.style.display = "none";
+    loginStudentPage.style.display = "none";
+    registerTutorPage.style.display = "block";
+    registerStudentPage.style.display = "none";
+    tutorReviewPage.style.display = "none";
+    addReviewPage.style.display = "none";
+    tutorPage.style.display = "none";
 }
 
-function loadTutorReview() {
-    homePage.style.display="none";
-    loginTutorPage.style.display="none";
-    loginStudentPage.style.display="none";
-    registerTutorPage.style.display="none";
-    registerStudentPage.style.display="none";
-    tutorReviewPage.style.display="block";
-    addReviewPage.style.display="none";
-    tutorPage.style.display="none";
+function loadTutorReview(tutorID) {
+    homePage.style.display = "none";
+    loginTutorPage.style.display = "none";
+    loginStudentPage.style.display = "none";
+    registerTutorPage.style.display = "none";
+    registerStudentPage.style.display = "none";
+    tutorReviewPage.style.display = "block";
+    addReviewPage.style.display = "none";
+    tutorPage.style.display = "none";
+    loadReviewDB(tutorID);
+
 }
 
 function loadAddTutorReview() {
-    homePage.style.display="none";
-    loginTutorPage.style.display="none";
-    loginStudentPage.style.display="none";
-    registerTutorPage.style.display="none";
-    registerStudentPage.style.display="none";
-    tutorReviewPage.style.display="none";
-    addReviewPage.style.display="block";
-    tutorPage.style.display="none";
+    homePage.style.display = "none";
+    loginTutorPage.style.display = "none";
+    loginStudentPage.style.display = "none";
+    registerTutorPage.style.display = "none";
+    registerStudentPage.style.display = "none";
+    tutorReviewPage.style.display = "none";
+    addReviewPage.style.display = "block";
+    tutorPage.style.display = "none";
+
 }
 
 function loadTutors() {
-    homePage.style.display="none";
-    loginTutorPage.style.display="none";
-    loginStudentPage.style.display="none";
-    registerTutorPage.style.display="none";
-    registerStudentPage.style.display="none";
-    tutorReviewPage.style.display="none";
-    addReviewPage.style.display="none";
-    tutorPage.style.display="block";
+    homePage.style.display = "none";
+    loginTutorPage.style.display = "none";
+    loginStudentPage.style.display = "none";
+    registerTutorPage.style.display = "none";
+    registerStudentPage.style.display = "none";
+    tutorReviewPage.style.display = "none";
+    addReviewPage.style.display = "none";
+    tutorPage.style.display = "block";
+    loadTutorsDB();
 }
 
 /* Loads current users and adds them to the page. */
-function loadUsers() {
+function loadTutorsDB() {
     //Set up XMLHttpRequest
     let xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = () => {//Called when data returns from server
         if (xhttp.readyState == 4 && xhttp.status == 200) {
             //Convert JSON to a JavaScript object
-            let usrArr = JSON.parse(xhttp.responseText);
+            usrArr = JSON.parse(xhttp.responseText);
 
-            //Return if no users
-            if(usrArr.length === 0)
+
+            //Return if tutors
+            if (usrArr.length === 0)
                 return;
 
             //Build string with user data
-            let htmlStr = "<table><tr><th>ID</th><th>Name</th><th>Email</th><th>Age</th></tr>";
-            for(let key in usrArr){
-                htmlStr += ("<tr><td>" + key + "</td><td>" + usrArr[key].name + "</td>");
-                htmlStr += ("<td>" + usrArr[key].email + "</td><td>" + usrArr[key].age + "</td></tr>");
+            let htmlStr = '<div class="card mb-3 row justify-content-center cardTutor">';
+            for (let key in usrArr) {
+                htmlStr += ('<div class="card-body allTutorCards">');
+                htmlStr += ('<h5 class="card-title text-center">' + usrArr[key].name + '</h5>');
+                htmlStr += (' <p class="card-text tutorText">Subjects: ' + usrArr[key].subject + '</p>');
+                htmlStr += (' <p class="card-text tutorText">Qualification: ' + usrArr[key].qualification + '</p>');
+                htmlStr += (' <p class="card-text tutorText">Region: ' + usrArr[key].region + '</p>');
+                htmlStr += (' <p class="card-text tutorText">Address: ' + usrArr[key].address + '</p>');
+                htmlStr += (' <p class="card-text tutorText">Phone Number: ' + usrArr[key].phonenum + '</p>');
+                htmlStr += (' <p class="card-text tutorText">Email: ' + usrArr[key].email + '</p>');
+                htmlStr += ('<div class="tutorButton">');
+                htmlStr += ('<a class="btn btn-primary" onclick="loadAddTutorReview(\'' + usrArr[key].tutorID + '\')">Add Reviews</a>');
+                htmlStr += ('<a class="btn btn-primary" onclick="loadTutorReview(\'' + usrArr[key].tutorID + '\')">Reviews</a>');
+                htmlStr += ('</div>');
+                htmlStr += (' </div>');
             }
             //Add users to page.
-            htmlStr += "</table>";
-            userDiv.innerHTML = htmlStr;
+            htmlStr += "</div>" +
+                "</div>";
+            tutorDiv.innerHTML = htmlStr;
         }
     };
 
     //Request data from all users
-    xhttp.open("GET", "/users", true);
+    xhttp.open("GET", "/tutors", true);
     xhttp.send();
+}
+
+function loadReviewDB(tutorID) {
+
+    let tutor = {
+        id: tutorID
+    };
+    //Set up XMLHttpRequest
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = () => {//Called when data returns from server
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+            //Convert JSON to a JavaScript object
+            let ReviewArr = JSON.parse(xhttp.responseText);
+
+            //Build string with user data
+            let htmlStr = '<div class="card mb-3 row justify-content-center cardTutor">';
+            for (let key in usrArr) {
+                if (usrArr[key].tutorID == tutorID) {
+                    htmlStr += ('<div class="card-body ">');
+                    htmlStr += ('<h5 class="card-title text-center">' + usrArr[key].name + '</h5>');
+                    htmlStr += (' <p class="card-text tutorText">Subjects: ' + usrArr[key].subject + '</p>');
+                    htmlStr += (' <p class="card-text tutorText">Qualification: ' + usrArr[key].qualification + '</p>');
+                    htmlStr += (' <p class="card-text tutorText">Region: ' + usrArr[key].region + '</p>');
+                    htmlStr += (' <p class="card-text tutorText">Address: ' + usrArr[key].address + '</p>');
+                    htmlStr += (' <p class="card-text tutorText">Phone Number: ' + usrArr[key].phonenum + '</p>');
+                    htmlStr += (' <p class="card-text tutorText">Email: ' + usrArr[key].email + '</p>');
+                    htmlStr += ('</div>');
+                    htmlStr += (' </div>');
+                    htmlStr += (' <h2 class="text-center">Reviews</h2>');
+                }
+            }
+
+            for (let key in ReviewArr) {
+                htmlStr += ('<div class="card-body justify-content-center cardTutor">');
+                htmlStr += ('<div class="card">');
+                htmlStr += (' <div class="card-body">');
+                htmlStr += (' <h5 class="card-title">'+ReviewArr[key].username+'</h5>');
+                htmlStr += (' <p class="card-text">Rating:'+ReviewArr[key].rating+'</p>');
+                htmlStr += ('<p class="card-text">Comments:'+ReviewArr[key].comment+'</p>');
+                htmlStr += ('</div>');
+                htmlStr += (' </div>');
+
+            }
+            //Add users to page.
+            htmlStr += '<div class="backButton">\n' +
+                '            <button class="btn btn-primary btn-lg btn-block" onclick="loadTutors()">Back</button>\n' +
+                '        </div>\n' +
+                '    </div>\n' +
+                '</div>';
+            reviewTutorDiv.innerHTML = htmlStr;
+        }
+
+    };
+    xhttp.open("POST", "/reviewstutor", true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+    xhttp.send(JSON.stringify(tutor));
 }
 
 
@@ -163,27 +240,26 @@ function registerStudent() {
         email: usrEmail,
         pass: usrPass
     };
-    
+
     //Set up function that is called when reply received from server
-    xhttp.onreadystatechange = function() {
+    xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
 
-            if(xhttp.responseText=="success"){
+            if (xhttp.responseText == "success") {
                 toastr.success("User added successfully, redirecting to login");
-                setTimeout(loadStudentLogin,2000);
-            }else{
+                setTimeout(loadStudentLogin, 2000);
+            } else {
                 toastr.warning("User already exist please login");
-                setTimeout(loadStudentLogin,2000);
+                setTimeout(loadStudentLogin, 2000);
             }
-        }
-        else{
+        } else {
         }
     };
 
     //Send new user data to server
     xhttp.open("POST", "/registerstudent", true);
     xhttp.setRequestHeader("Content-type", "application/json");
-    xhttp.send( JSON.stringify(student) );
+    xhttp.send(JSON.stringify(student));
 }
 
 /* Posts a new user to the server. */
@@ -197,48 +273,47 @@ function registerTutor() {
     let usrEmail = document.getElementsByName("email")[1].value;
     let usrPass = document.getElementsByName("password")[1].value;
     let usrQualification = document.getElementsByName("qualification")[0].value;
-    let usrAddress=document.getElementsByName("address")[0].value;
+    let usrAddress = document.getElementsByName("address")[0].value;
     let e = document.getElementById("region");
     let usrRegion = e.options[e.selectedIndex].text;
     let f = document.getElementById("grade");
     let usrGrade = f.options[e.selectedIndex].text;
-    let usrSubjects=document.getElementsByName("subjects")[0].value;
-    let usrPhone=document.getElementsByName("phone")[0].value;
+    let usrSubjects = document.getElementsByName("subjects")[0].value;
+    let usrPhone = document.getElementsByName("phone")[0].value;
 
     //Create object with user data
     let tutor = {
         name: usrName,
-        username:usrUsername,
+        username: usrUsername,
         email: usrEmail,
         pass: usrPass,
-        qualification:usrQualification,
-        address:usrAddress,
-        region:usrRegion,
-        class:usrGrade,
-        subjects:usrSubjects,
-        phone:usrPhone
+        qualification: usrQualification,
+        address: usrAddress,
+        region: usrRegion,
+        class: usrGrade,
+        subjects: usrSubjects,
+        phone: usrPhone
     };
 
     console.log(tutor);
 
     //Set up function that is called when reply received from server
-    xhttp.onreadystatechange = function() {
+    xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
 
-            if(xhttp.responseText=="success"){
+            if (xhttp.responseText == "success") {
                 toastr.success("User added successfully, redirecting to login");
-                setTimeout(loadTutorLogin,2000);
-            }else{
+                setTimeout(loadTutorLogin, 2000);
+            } else {
                 toastr.warning("User already exist please login");
-                setTimeout(loadTutorLogin,2000);
+                setTimeout(loadTutorLogin, 2000);
             }
-        }
-        else{
+        } else {
         }
     };
 
     //Send new user data to server
     xhttp.open("POST", "/registertutor", true);
     xhttp.setRequestHeader("Content-type", "application/json");
-    xhttp.send( JSON.stringify(tutor) );
+    xhttp.send(JSON.stringify(tutor));
 }
